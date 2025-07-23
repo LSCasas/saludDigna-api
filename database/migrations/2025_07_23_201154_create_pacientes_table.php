cd73be8a4dc43a78cf7ecd0d22f20e52fbbd259f
Luis Casas <luisscasas45@gmail.com>
2025-07-23 14:19:37 -0600

    FEAT/ Add Paciente model and migration for pacientes table

---
+++ database/migrations/2025_07_23_201154_create_pacientes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public $withinTransaction = false;
+    public function up()
+    {
+        Schema::create('pacientes', function (Blueprint $table) {
+            $table->id('id_paciente');
+            $table->string('nombre');
+            $table->string('apellidoP');
+            $table->string('apellidoM');
+            $table->string('genero', 10);
+            $table->date('fecha_nacimiento');
+            $table->string('telefono')->nullable();
+            $table->string('correo')->unique()->nullable();
+            $table->string('direccion')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pacientes');
+    }
+};
